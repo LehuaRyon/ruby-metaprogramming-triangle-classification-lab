6fb0ebcae4c539125014671349b7422472479509
Jasmine Ryon <jryon12@priorypride.net>
2021-03-18 14:17:19 -0700

    commit tree ecb6ab9dc95d5a93eb32fd058b128ecbc2ffcdf5

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,35 @@
 class Triangle
-  # write code here
-end
+  attr_accessor :a, :b, :c
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    confirm_triangle
+    #euilateral: else: 3 equal sides : check to see if side 1 = side 2, if side 2 = side 3
+    #isosceles: elsif: 2 equal sides : if side 1 = side 2or if side 1 = side 3 or if side 2 = side 3
+    #scalene: else: no equal sides
+    if a == b && b == c
+      :equilateral
+    elsif a == b || a == c || b == c
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  def confirm_triangle
+    triangle = [(a + b > c), (a + c > b), (b + c > a)]
+    [a, b, c].each do |side|
+      triangle << false if side <= 0 
+    raise TriangleError if triangle.include?(false)
+    end
+  end
+
+  class TriangleError < StandardError
+    # triangle error code
+  end
+end 
